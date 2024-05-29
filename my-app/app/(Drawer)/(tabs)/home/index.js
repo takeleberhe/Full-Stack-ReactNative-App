409a22c4325f78a790e35203155fd428c0add1e6
takeleberhe <77755440+takeleberhe@users.noreply.github.com>
2024-05-29 18:14:43 +0300

    performance optimized using useCallBack React.memo

--- my-app/app/(Drawer)/(tabs)/home/index.js
+++ my-app/app/(Drawer)/(tabs)/home/index.js
@@ -1,10 +1,18 @@
-import React, { useContext } from "react";
+import React, {
+  useContext,
+  useCallback,
+  useMemo,
+  memo,
+  lazy,
+  Suspense,
+} from "react";
 import { View } from "react-native";
 import { Stack } from "expo-router";
 import ProductList from "../../../Components/ProductList";
 import AuthContext from "../../../../Context/AuthContext/AuthContext";
 import SearchBar from "../../../Components/SearchBar";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { isEqual } from "lodash";
+//const SearchBar = lazy(() => import("../../../Components/SearchBar"));
 
 const Homepage = () => {
   const {
@@ -12,8 +20,8 @@ const Homepage = () => {
     searchstate: { searchQuery },
   } = useContext(AuthContext);
 
-  /* filter products */
-  const transformedproduct = () => {
+  /* performance optimazation using useCallback hook */
+  const memoizedProducts = useCallback(() => {
     let filteredProducts = product;
     if (searchQuery) {
       filteredProducts = filteredProducts.filter((prod) => {
@@ -21,20 +29,20 @@ const Homepage = () => {
       });
     }
     return filteredProducts;
-  };
+  }, [product]);
 
   return (
     <View className="flex-1 items-center">
       {/* custom Search component */}
       <Stack.Screen
         options={{
-          header: () => <SearchBar />,
+          header: () =><SearchBar />
         }}
       ></Stack.Screen>
       {/* product list custom component */}
-      <ProductList data={transformedproduct} />
+      <ProductList data={memoizedProducts} />
     </View>
   );
 };
 
-export default Homepage;
+export default memo(Homepage, isEqual);
