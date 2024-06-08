314bf7844375c7787bf1435aafdfd6bb07ce437d
takeleberhe <77755440+takeleberhe@users.noreply.github.com>
2024-06-08 17:46:36 +0300

    refactore

--- my-app/app/Components/SearchBar.js
+++ my-app/app/Components/SearchBar.js
@@ -5,7 +5,7 @@ import AuthContext from "../../Context/AuthContext/AuthContext";
 const SearchBar = () => {
   const { searchDispatch } = useContext(AuthContext);
   return (
-    <SafeAreaView className="flex-1 rounded-5xl p-5 mb-10 w-full h-9 ">
+    <SafeAreaView className="flex-1 rounded-5xl p-5 w-full h-9 ">
       <StatusBar style="light" backgroundColor="blue" />
       <TextInput
         className="text-1xl p-2 h-20 w-full rounded-md bg-slate-200 mb-5"
@@ -23,21 +23,4 @@ const SearchBar = () => {
     </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  inputText: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    boarderColor: "black",
-    backgroundColor: "gray",
-    boarderWidth: 1,
-    borderRadius: 5,
-    height: 40,
-    width: "100%",
-    marginBottom: 50,
-  },
-});
 export default SearchBar;
