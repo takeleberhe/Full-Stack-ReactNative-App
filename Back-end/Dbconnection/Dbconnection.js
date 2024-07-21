f77057b0e97df1f9d0fcc227511c276ff5e9c255
takeleberhe <77755440+takeleberhe@users.noreply.github.com>
2024-07-21 07:14:21 +0300

    refactor

--- Back-end/Dbconnection/Dbconnection.js
+++ Back-end/Dbconnection/Dbconnection.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
 
     console.log("+++++++database connected++++++++ ");
   } catch (e) {
-    console.log("Error==========onDb =======connection!");
+    console.log(e.message);
   }
 };
 
